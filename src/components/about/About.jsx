77591c6e175179982dc2859c886a95827d08c476
1Shubham7 <shubhammahar1306@gmail.com>
2023-07-19 23:29:48 +0530

    preping about.png

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,21 +5,11 @@ export default function About() {
   return (
     <>
     <div className="section one">
-
-    {/* for the horizontal devision */}
-        <div className="container">
       <div className="sec top-text">
         Coding is fun when you have friends around to cheer you!
       </div>
-      <div className="sec rectange">twitter</div>
       </div>
 
-
-      </div>
-      <div className="section two">
-      <div className="top-text">
-      </div>
-      </div>
     </>
   )
 }
