4fe641327f47271fbd5ff8c912025c11f81ca36f
1Shubham7 <shubhammahar1306@gmail.com>
2023-07-19 23:39:02 +0530

    found the right fit image (about)

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,9 +4,11 @@ import './about.css'
 export default function About() {
   return (
     <>
-    <div className="section one">
+    <div className="section">
+        <div className="container">
       <div className="sec top-text">
         Coding is fun when you have friends around to cheer you!
+      </div>
       </div>
       </div>
 
