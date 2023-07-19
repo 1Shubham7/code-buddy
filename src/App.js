c17caf1ea847ab4ded05fa124983e49e6207e260
1Shubham7 <shubhammahar1306@gmail.com>
2023-07-19 13:29:52 +0530

    components working fine till now

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Navbar from "./components/navbar/Navbar";
 // import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       hi
+      <Navbar />
     </div>
   );
 }
