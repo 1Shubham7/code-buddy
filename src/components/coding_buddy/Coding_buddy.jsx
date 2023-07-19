8bea0dc3cb0a91e56ee000485e3dde0b700cbf64
1Shubham7 <shubhammahar1306@gmail.com>
2023-07-19 20:36:59 +0530

    image transparency + prefect with navbar

--- src/components/coding_buddy/Coding_buddy.jsx
+++ src/components/coding_buddy/Coding_buddy.jsx
@@ -4,9 +4,13 @@ import bg_image from './bg_image.png';
 
 export default function Coding_buddy() {
   return (
+
+
     <div className="website-head">
       <div className='image-container'>
+        <div className="image-div">
       <img src={bg_image} alt="pic" className='fit-to-screen' />
+      </div>
       </div>
     </div>
   )
