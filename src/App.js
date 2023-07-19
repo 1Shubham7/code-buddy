d3cf0326aaf8c8bfbad75ec192302507055248e7
1Shubham7 <shubhammahar1306@gmail.com>
2023-07-19 13:12:59 +0530

    contentional way, back with src files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+function App() {
+
+  return (
+    <div className="App">
+      hi
+    </div>
+  );
+}
+
+export default App;
