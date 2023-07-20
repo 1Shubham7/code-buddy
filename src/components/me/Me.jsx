6ea7c8bf008de1160db618d1b04b09c54925c041
Shubham Singh <shubhammahar1306@gmail.com>
2023-07-20 22:06:46 +0530

    me section complete

--- src/components/me/Me.jsx
+++ src/components/me/Me.jsx
@@ -6,12 +6,19 @@ export default function Me() {
   return (
     <> 
     <div className="segment">
-      <p className="my-text">
-        HASDADFadsf
-      </p>
+      {/* <div className="flexarea"> */}
+      <div className="my-text">
+        <p className="love">Made with Love by</p>
+        <p className="name">Shubham Singh|</p>
+        <p className="description">GSSoC'23 | Web Dev | Dev Ops | Android dev |
+           Open-source Enthusiast | Technical Writer |
+            Postman Student Expert | First year Undergrad 
+            (Computer Science Eng.)</p>
       
+      </div>
     <img src={myPic} alt="hi" className="pic"/>
-    </div>
+      </div>
+    {/* </div> */}
     </>
   )
 }
