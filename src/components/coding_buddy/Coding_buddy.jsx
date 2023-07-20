24b2efb112735c6cb3df7eefc5cbcc2ca05c2c1a
Shubham Singh <shubhammahar1306@gmail.com>
2023-07-20 17:50:25 +0530

    creating the typing text animation

--- src/components/coding_buddy/Coding_buddy.jsx
+++ src/components/coding_buddy/Coding_buddy.jsx
@@ -3,13 +3,22 @@ import './coding_buddy.css'
 import bg_image from './bg_image.png';
 
 export default function Coding_buddy() {
+const textDisplay = document.getElementsByClassName('main-text')
+const phrases = ["Find you Coding Buddy", "Participate in Hackathons, Competitions and events together", "Build projects together and get hired"]
+
+function loop() {
+  
+}
+
   return (
 
 
     <div className="website-head">
       <div className='image-container'>
         <div className="image-div">
-          <div className="main-text">HELLO</div>
+          <div className="main-text" >
+            
+          </div>
       </div>
       </div>
     </div>
