4f04c16ab15df2553136aa6afcf6cc80e401d67d
Shubham Singh <shubhammahar1306@gmail.com>
2023-07-20 18:42:35 +0530

    animation error

--- src/components/coding_buddy/Coding_buddy.jsx
+++ src/components/coding_buddy/Coding_buddy.jsx
@@ -1,33 +1,39 @@
 import React from 'react'
 import './coding_buddy.css'
-import bg_image from './bg_image.png';
+// import bg_image from './bg_image.png';
+
 
 export default function Coding_buddy() {
-const textDisplay = document.getElementById('tt')
-const phrases = ["Find you Coding Buddy", "Participate in Hackathons, Competitions and events together", "Build projects together and get hired"]
+const textDisplay = document.getElementById('tt');
+const phrases = ["Find you Coding Buddy", "Participate in Hackathons, Competitions and events together", "Build projects together and get hired"];
 
 let i=0
 let j=0
 let currentPhrase = []
 let isDeleting = false
+let isEnd = false
 
-function loop() {
-  textDisplay.innerHTML = currentPhrase.join("")
+const loop = ()=> {
+
+ isEnd =  false
+  textDisplay.innerHTML = currentPhrase.join('')
 
   if (i<phrases.length) {
-
   if (!isDeleting && j <= phrases[i].length){
     currentPhrase.push(phrases[i][j])
     j++
+    textDisplay.innerHTML = currentPhrase.join('')
   }
 
   if (isDeleting  && j<= phrases[i].length){
     currentPhrase.pop(phrases[i][j])
     j--
+    textDisplay.innerHTML = currentPhrase.join('')
   }
 
 
   if (j === phrases[i].length){
+    isEnd = true
     isDeleting = true
   }
 
@@ -35,13 +41,21 @@ function loop() {
     currentPhrase = []
     isDeleting = false
     i++
-  }
 
+    if (i=== phrases.length){
+      i=0
+    }
+  }
 }
-setTimeout(loop, 500)
+const spedUp = Math.random() * (80-50) + 50
+const normalSpeed = Math.random() * (300-200) + 200
+const time = isEnd ? 2000: isDeleting ? spedUp : normalSpeed
+setTimeout(loop, time)
 }
 
 loop()
+
+
 
   return (
 
