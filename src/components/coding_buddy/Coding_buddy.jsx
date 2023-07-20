12f77f7b8419574a5e764636d74d989b595ee350
Shubham Singh <shubhammahar1306@gmail.com>
2023-07-20 18:16:55 +0530

    typing animation progress

--- src/components/coding_buddy/Coding_buddy.jsx
+++ src/components/coding_buddy/Coding_buddy.jsx
@@ -3,12 +3,45 @@ import './coding_buddy.css'
 import bg_image from './bg_image.png';
 
 export default function Coding_buddy() {
-const textDisplay = document.getElementsByClassName('main-text')
+const textDisplay = document.getElementById('tt')
 const phrases = ["Find you Coding Buddy", "Participate in Hackathons, Competitions and events together", "Build projects together and get hired"]
 
+let i=0
+let j=0
+let currentPhrase = []
+let isDeleting = false
+
 function loop() {
-  
+  textDisplay.innerHTML = currentPhrase.join("")
+
+  if (i<phrases.length) {
+
+  if (!isDeleting && j <= phrases[i].length){
+    currentPhrase.push(phrases[i][j])
+    j++
+  }
+
+  if (isDeleting  && j<= phrases[i].length){
+    currentPhrase.pop(phrases[i][j])
+    j--
+  }
+
+
+  if (j === phrases[i].length){
+    isDeleting = true
+  }
+
+  if (isDeleting && j === 0) {
+    currentPhrase = []
+    isDeleting = false
+    i++
+  }
+
 }
+setTimeout(loop, 500)
+}
+
+loop()
 
   return (
 
@@ -17,7 +50,7 @@ function loop() {
       <div className='image-container'>
         <div className="image-div">
           <div className="main-text" >
-            
+            <p id="tt"></p>
           </div>
       </div>
       </div>
