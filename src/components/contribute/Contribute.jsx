06376d5b78c88fd9307288eef47ed87bcc51d4c3
Shubham Singh <shubhammahar1306@gmail.com>
2023-07-21 12:50:18 +0530

    changed the contributions text

--- src/components/contribute/Contribute.jsx
+++ src/components/contribute/Contribute.jsx
@@ -12,13 +12,10 @@ export default function Contribute() {
         </h1>
         <p> - The simplest way to contribute is to give our 
           repository a star on GitHub</p>
-        <p>We would love our contriubtors, they are the once who 
-          make it possible to our webapp. Even you can contribute 
-          to the website and help the community. Contributions are not
+        <p>Did you find the project useful and you wish to contribute ? then please Contribute and help the project grow. Your small contribution will effect the experiences of hundreds of users using the website. Contributions are not
            just about code, you can even contribute through documentation, 
            Graphic designing, and many more ways. To know more about contributing, <a href="https://github.com/1Shubham7/code-buddy">Click here</a></p>
       </div>
-      
     </div>
   )
 }
