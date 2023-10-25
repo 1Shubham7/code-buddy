fcbd81a47c7b8e9fffc31866107724bcd91639a3
Shubham Singh <shubhammahar1306@gmail.com>
2023-10-25 20:02:50 +0530

    add: addCoder() ready + added validator, primitive, time etc.

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -1,5 +1,43 @@
 package routes
 
-func AddCoder(){
-	
+import (
+	"fmt"
+	"net/http"
+	"time"
+	"context"
+	"github.com/1shubham7/code-buddy/models"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"github.com/go-playground/validator/v10"
+)
+
+var validate = validate.New()
+var coderCollection *mongo.Collection = OpenCollection(Client, "coder")
+
+func AddCoder(c *gin.Context) {
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	var coder models.Coder
+
+	if err := c.BindJSON(&coder); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+	validationErr := validate.Struct(coder)
+	if validationErr != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": validationErr.Error()})
+		fmt.Println(validationErr)
+		return
+	}
+	coder.ID = primitive.NewObjectID()
+	result, insertErr := coderCollection.InsertOne(ctx, coder)
+	if insertErr != nil {
+		msg := fmt.Sprintf("Coder account not created")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+		fmt.Println(insertErr)
+		return
+	}
+	defer cancel()
+	c.JSON(http.StatusOK, result)
 }
