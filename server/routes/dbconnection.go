9925ccd9ba160f15e6a41ec00cb11a5c624be582
Shubham Singh <shubhammahar1306@gmail.com>
2023-10-25 18:53:16 +0530

    add: completed connection.go

--- server/routes/dbconnection.go
+++ server/routes/dbconnection.go
@@ -28,3 +28,7 @@ func DBinstance() *mongo.Client {
 
 var Client *mongo.Client = DBinstance()
 
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection{
+	var collection *mongo.Collection = client.Database("coderdb").Collection(collectionName)
+	return collection
+}
