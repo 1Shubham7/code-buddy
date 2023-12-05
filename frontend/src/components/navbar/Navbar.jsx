f3ccc21909c987a56dcc24d785e5679c24e832fd
Harrison <harrisonjones5678@gmail.com>
2023-12-04 22:35:16 -0500

    use margin and change value

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,10 +1,10 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 
 export default function Navbar() {
 
-    const [toggleMenu, setToggleMenu] = useState(false);
+  const [toggleMenu, setToggleMenu] = useState(false);
 
   return (
     <div className="navbar">
@@ -25,18 +25,16 @@ export default function Navbar() {
           ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
           : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
         {toggleMenu && (
-        <div className="navbar-menu_container scale-up-center">
-          <div className="navbar-menu_container-links">
-            <p><a href="#home">Coding Buddy</a></p>
-            <p><a href="#facb">Find a Coding Buddy</a></p>
-            <p><a href="#about">About</a></p>
-            <p><a href="#community">Community</a></p>
-            <p><a href="https://github.com/1Shubham7/code-buddy">GitHub</a></p>
+          <div className="navbar-menu_container scale-up-center">
+            <div className="navbar-menu_container-links">
+              <p><a href="#home">Coding Buddy</a></p>
+              <p><a href="#facb">Find a Coding Buddy</a></p>
+              <p><a href="#about">About</a></p>
+              <p><a href="#community">Community</a></p>
+              <p><a href="https://github.com/1Shubham7/code-buddy">GitHub</a></p>
+            </div>
+
           </div>
-          <div className="navbar-menu_container-links-sign">
-            <button type="button">Log In</button>
-          </div>
-        </div>
         )}
       </div>
     </div>
