7c6e5134aad54cfc1952e2cd8fa69a8ac5483f81
Shubham Singh <shubhammahar1306@gmail.com>
2023-12-12 09:28:58 +0530

    Delete frontend/node_modules directory

--- frontend/node_modules/@mui/material/Paper/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { default } from './Paper';
-export type { PaperProps, PaperPropsVariantOverrides, PaperTypeMap, PaperOwnProps } from './Paper';
-
-export { default as paperClasses } from './paperClasses';
-export * from './paperClasses';
